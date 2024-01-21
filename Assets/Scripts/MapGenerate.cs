5e59848e5789645c253aeb41918fa68f4319376d
nanfeng979 <306338401@qq.com>
2024-01-21 15:41:37 +0800

    使用json读取预制体

--- Assets/Scripts/MapGenerate.cs
+++ Assets/Scripts/MapGenerate.cs
@@ -5,15 +5,18 @@ using UnityEngine;
 public abstract class MapGenerate : MonoBehaviour
 {
     #region 地图相关
-    [SerializeField]
-    private List<GameObject> mapBlocks;
+    private List<GameObject> mapBlocks = new List<GameObject>();
     private int mapXCount;
     private int mapZCount;
     private int[] mapIndex;
     public string mapFileName;
+    #region 障碍物相关
     [SerializeField]
     private List<GameObject> obstacles;
-    #endregion
+    private List<int> obstaclesIndex = new List<int>();
+    private List<float> obstaclesYOffSet = new List<float>();
+    #endregion 障碍物相关
+    #endregion 地图相关
 
     #region 玩家相关
     public GameObject playerPrefab;
@@ -28,13 +31,13 @@ public abstract class MapGenerate : MonoBehaviour
             return;
         }
         MapGenerateData mapGenerateData = Y9g.SystemIO.ReadJsonFromStreamingAssets<MapGenerateData>(mapFileName);
-        Init(mapGenerateData.mapXCount, mapGenerateData.mapZCount, mapGenerateData.mapIndex);
+        Init(mapGenerateData);
 
         // 创建地图。
         GameObject mapList = Y9g.MapGenerate.GenerateVariousMap(mapXCount, mapZCount, mapBlocks, mapIndex, 0.2f, true);
 
         // 绑定地图。
-        MapManager.Instance.SetBaseInfo(mapXCount, mapZCount, mapIndex, mapList, mapGenerateData.obstacles);
+        MapManager.Instance.SetBaseInfo(mapXCount, mapZCount, mapIndex, mapList, obstaclesIndex);
 
         // 创建障碍物。
         // 遍历maplist
@@ -45,7 +48,7 @@ public abstract class MapGenerate : MonoBehaviour
             {
                 // 创建障碍物。
                 GameObject obstacle = Instantiate(obstacles[mapIndex[i]], mapList.transform.GetChild(i).position, Quaternion.identity, mapList.transform.GetChild(i));
-                obstacle.transform.position = Y9g.UsualCalculate.Vector3ChangeY(obstacle.transform.position, 0.5f);
+                obstacle.transform.position = Y9g.UsualCalculate.Vector3ChangeY(obstacle.transform.position, obstaclesYOffSet[mapIndex[i] - 1]);
             }
         }
 
@@ -60,20 +63,37 @@ public abstract class MapGenerate : MonoBehaviour
         MapManager.Instance.SetPlayerIndex(0);
     }
 
-    private void Init(int mapXCount, int mapZCount, int[] mapIndex)
+    private void Init(MapGenerateData mapGenerate)
     {
-        this.mapXCount = mapXCount;
-        this.mapZCount = mapZCount;
-        this.mapIndex = mapIndex;
+        this.mapXCount = mapGenerate.mapXCount;
+        this.mapZCount = mapGenerate.mapZCount;
+        this.mapIndex = mapGenerate.mapIndex;
+        for (int i = 0; i < mapGenerate.mapBlocks.Count; i++)
+        {
+            this.mapBlocks.Add(Resources.Load<GameObject>(mapGenerate.mapBlocks[i]));
+        }
+        for (int i = 0; i < mapGenerate.obstaclesInfo.Count; i++)
+        {
+            this.obstaclesIndex.Add(mapGenerate.obstaclesInfo[i].index);
+            this.obstaclesYOffSet.Add(mapGenerate.obstaclesInfo[i].yOffSet);
+        }
     }
     
 }
 
-[System.Serializable]
+[Serializable]
 public class MapGenerateData
 {
     public int mapXCount;
     public int mapZCount;
     public int[] mapIndex;
-    public List<int> obstacles;
+    public List<string> mapBlocks;
+    public List<ObstacleInfo> obstaclesInfo;
+}
+
+[Serializable]
+public class ObstacleInfo
+{
+    public int index;
+    public float yOffSet;
 }
