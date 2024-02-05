460be66cbd2c40f91f414e59e84efd3857df7978
nanfeng979 <306338401@qq.com>
2024-02-05 18:28:01 +0800

    实现切换语言

--- Assets/Scripts/LanguageManager.cs
+++ Assets/Scripts/LanguageManager.cs
@@ -1,27 +1,34 @@
 using System.Collections.Generic;
 using Y9g;
 using Newtonsoft.Json;
+using UnityEngine;
 
 public class LanguageManager : Singleton<LanguageManager>
 {
+    // 语言文本字典。
     private Dictionary<string, string> languageDict = new Dictionary<string, string>();
+
+    // 观察者列表。
+    private List<UIText> observers = new List<UIText>();
 
     private void Start() {
         SetLanguageType(LanguageType.zh);
     }
 
     /// <summary>
-    /// 加载指定的语言文本
+    /// 加载指定的语言文本。
     /// </summary>
     /// <param name="languageType"> 语言类型 </param>
     private void LoadLanguageType(LanguageType languageType)
     {
         string jsonText = SystemIO.ReadJsonFromStreamingAssets("Language_" + languageType.ToString());
         languageDict = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonText);
+        // 通知观察者。
+        NotifyObservers();
     }
 
     /// <summary>
-    /// 设置语言
+    /// 改变语言类型。
     /// </summary>
     /// <param name="languageType"> 语言类型 </param>
     public void SetLanguageType(LanguageType languageType)
@@ -37,10 +44,42 @@ public class LanguageManager : Singleton<LanguageManager>
         }
         return key + " not found";
     }
+
+    /// <summary>
+    /// 通知观察者。
+    /// </summary>
+    private void NotifyObservers()
+    {
+        foreach (var observer in observers)
+        {
+            observer.OnLanguageChanged();
+        }
+    }
+
+    /// <summary>
+    /// 注册观察者。
+    /// </summary>
+    /// <param name="observer"> 观察者 </param>
+    public void RegisterObserver(UIText observer)
+    {
+        if (!observers.Contains(observer))
+        {
+            observers.Add(observer);
+        }
+    }
+
+    /// <summary>
+    /// 取消注册观察者。
+    /// </summary>
+    /// <param name="observer"> 观察者 </param>
+    public void UnregisterObserver(UIText observer)
+    {
+        observers.Remove(observer);
+    }
 }
 
 public enum LanguageType
 {
     zh,
-    en
+    en,
 }
--- Assets/Scripts/UIText.cs
+++ Assets/Scripts/UIText.cs
@@ -11,5 +11,12 @@ public class UIText : MonoBehaviour
     {
         text = GetComponent<Text>();
         text.text = LanguageManager.Instance.GetText(key);
+
+        LanguageManager.Instance.RegisterObserver(this);
+    }
+
+    public void OnLanguageChanged()
+    {
+        text.text = LanguageManager.Instance.GetText(key);
     }
 }
